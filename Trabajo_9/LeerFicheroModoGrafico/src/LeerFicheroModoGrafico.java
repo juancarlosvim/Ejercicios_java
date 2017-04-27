d4f141e82754b2fa728bf9a72cb4297456e752e5
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2017-04-27 17:01:30 +0200

    Leer fichero [Modo Gráfico]
    Selecciona un archivo, y al pulsar leer el archivo lo lee

---
+++ Trabajo_9/LeerFicheroModoGrafico/src/LeerFicheroModoGrafico.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alumno
+ */
+public class LeerFicheroModoGrafico {
+
+    
+    public static void main(String[] args) {
+       InterfazGrafica interfazGrafica = new InterfazGrafica();
+       interfazGrafica.setLocationRelativeTo(null);
+       interfazGrafica.setVisible(true);
+    }
+    
+}
