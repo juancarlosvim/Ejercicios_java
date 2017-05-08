800f8fe628c37aa189aa10b812c0eb396e7c7b70
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2017-05-08 20:31:41 +0200

    Contraseña [Gráficos][NetBeans]

---
+++ Trabajo_8/prContraseña/src/PrContraseña.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author juancarlosvim
+ */
+public class PrContraseña {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Contraseña c = new Contraseña();
+        c.setVisible(true);
+    }
+    
+}
