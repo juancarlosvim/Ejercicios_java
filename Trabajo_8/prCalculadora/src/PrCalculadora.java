eaf13b35be3a84b893d400d832b8135e289d4c68
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2017-05-08 18:51:54 +0200

    Calculadora [NetBeans][Gráfico]

---
+++ Trabajo_8/prCalculadora/src/PrCalculadora.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author juancarlosvim
+ */
+public class PrCalculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        CalculadoraGráficos cal = new CalculadoraGráficos();
+        cal.setVisible(true);
+    }
+    
+}
