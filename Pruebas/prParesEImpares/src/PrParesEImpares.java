8ea7237c9164b9603cc1987386a6ca93dc5a3931
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2017-05-09 21:04:39 +0200

    Suma de pares e impares [Gráfico][NetBeans]

---
+++ Pruebas/prParesEImpares/src/PrParesEImpares.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author juancarlosvim
+ */
+public class PrParesEImpares {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PareseImpares p = new PareseImpares();
+        p.setVisible(true);
+    }
+    
+}
