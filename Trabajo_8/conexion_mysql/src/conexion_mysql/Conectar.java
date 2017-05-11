36c59064adbaeffc18c4d7973658c53759ae606e
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2017-05-11 17:53:45 +0200

    Añadido el select

--- Trabajo_8/conexion_mysql/src/conexion_mysql/Conectar.java
+++ Trabajo_8/conexion_mysql/src/conexion_mysql/Conectar.java
@@ -13,9 +13,19 @@ public class Conectar {
 			String url = "jdbc:mysql://localhost/practica";
 			Connection com = DriverManager.getConnection(url,"juancarlosvim","");
 			
+			Statement stm = com.createStatement();
+			ResultSet rs = stm.executeQuery("SELECT * FROM tabla1");
+			
+			if(rs.first()){
+				do{
+					System.out.println("ID "+ rs.getInt("id"));
+					System.out.println("Nombre "+ rs.getString("nombre"));
+				}while(rs.next());
+			}
+			
 			System.out.println("Conexion "+com);
 		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Bloque catch generado autom·ticamente
+			// TODO Bloque catch generado autom√°ticamente
 			e.printStackTrace();
 		}
 		
