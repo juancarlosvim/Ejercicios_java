4d647e92adf0e2c29ee8d30871a136aa50a8e969
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2017-05-15 16:59:24 +0200

    Mi primer Applet

---
+++ Trabajo_8/Applet/Applet01/src/Applet01.java
@@ -0,0 +1,14 @@
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Applet01 extends Applet {
+	
+	public void paint(Graphics g){
+		g.drawString("Hola Amigos",10,20);
+		
+		
+		g.setColor(Color.RED);
+		
+	}
+}
