a977021081c50a43c971c1e15b7665e37d251458
Juan Carlos <juancarlosvillegasmarmol@gmail.com>
2017-05-16 19:48:16 +0200

    Abstract
    Utilizando una clase abstracta Figura

---
+++ Pruebas/prAbstractFigura/src/Figura.java
@@ -0,0 +1,12 @@
+
+public abstract class Figura {
+	private double x;
+	private double y;
+	
+	public Figura(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public abstract double area();
+}
---
+++ Pruebas/prAbstractFigura/src/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Circulo c =new Circulo(5, 6, 5);
+		
+		System.out.println("Area del Circulo=>  "+c.area());
+		
+		
+		
+		Cuadrado cu= new Cuadrado(5, 5, 10);
+		
+		System.out.println("Area del Cuadrado=> "+cu.area());
+
+	}
+
+}
